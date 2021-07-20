028d9ad567e582dfe54d7f5c8ab8759dec5ee995
Alan <zg915@nyu.edu>
2021-07-20 15:54:28 +0800

    altering releasetime and description in projuct table

---
+++ springproject.sql
@@ -0,0 +1,82 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.7.21 : Database - springproject
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE `springproject`;
+
+/*Table structure for table `organization` */
+
+DROP TABLE IF EXISTS `organization`;
+
+CREATE TABLE `organization` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `position` varchar(32) NOT NULL,
+  `numbers` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `organization` */
+
+LOCK TABLES `organization` WRITE;
+
+insert  into `organization`(`id`,`name`,`position`,`numbers`) values (1,'产品部','上海',100),(2,'事业部','上海',79),(3,'人事部','上海',40);
+
+UNLOCK TABLES;
+
+/*Table structure for table `person` */
+
+DROP TABLE IF EXISTS `person`;
+
+CREATE TABLE `person` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `age` int(11) NOT NULL,
+  `gender` varchar(32) NOT NULL,
+  `address` varchar(32) DEFAULT NULL,
+  `mail` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `person` */
+
+LOCK TABLES `person` WRITE;
+
+insert  into `person`(`id`,`name`,`age`,`gender`,`address`,`mail`) values (1,'张三',24,'男','上海市浦东新区碧波路456号','123412341@qq.com'),(3,'吴亦凡',29,'男','北京市西城区','123476542@qq.com');
+
+UNLOCK TABLES;
+
+/*Table structure for table `product` */
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `description` varchar(164) DEFAULT NULL,
+  `releasetime` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `product` */
+
+LOCK TABLES `product` WRITE;
+
+insert  into `product`(`id`,`name`,`description`,`releasetime`) values (1,'PASV6.5','普元应用服务器软件','2021-07-19');
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
