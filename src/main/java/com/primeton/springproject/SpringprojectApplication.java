991fce0e212832267b6692c065f448509f6870f3
Alan <zg915@nyu.edu>
2021-07-28 17:33:12 +0800

    add: spring cloud eureka client

--- src/main/java/com/primeton/springproject/SpringprojectApplication.java
+++ src/main/java/com/primeton/springproject/SpringprojectApplication.java
@@ -2,8 +2,10 @@ package com.primeton.springproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringprojectApplication {
 
     public static void main(String[] args) {
