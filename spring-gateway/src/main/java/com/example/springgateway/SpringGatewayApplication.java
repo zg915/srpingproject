d7a455ceb1a23f70dfdf48faf274125c5bf93d61
Alan <zg915@nyu.edu>
2021-07-30 16:40:47 +0800

    add: spring gateway

---
+++ spring-gateway/src/main/java/com/example/springgateway/SpringGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.springgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGatewayApplication.class, args);
+    }
+
+}
